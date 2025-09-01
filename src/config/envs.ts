bbf1a6be416d99e8426cfbc9960f58e82171351a
Cristhian <crisrodam1996@gmail.com>
2025-09-01 16:48:28 -0500

    Conexión a la base de datos mediante variables de entorno

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -13,9 +13,17 @@ const getEnvVar = (name:string) => {
 
 
 const PORT : number = Number(process.env.PORT) || 3000
+const DB_HOST_DEV : string = getEnvVar("DB_HOST_DEV")
+const DB_NAME_DEV : string = getEnvVar("DB_NAME_DEV")
+const DB_PASSWORD_DEV : string = getEnvVar("DB_PASSWORD_DEV")
+const DB_USERNAME_DEV : string = getEnvVar("DB_USERNAME_DEV")
 
 
 export {
-  PORT
+  PORT,
+  DB_HOST_DEV,
+  DB_NAME_DEV,
+  DB_PASSWORD_DEV,
+  DB_USERNAME_DEV
 }
 
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,13 @@
 import { DataSource } from "typeorm";
+import { DB_HOST_DEV, DB_NAME_DEV, DB_PASSWORD_DEV, DB_USERNAME_DEV } from "../config/envs.js";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: DB_HOST_DEV,
   port: 5432,
-  username: "giftshuffle",
-  password: "giftshuffle2025",
-  database: "giftshufflebd",
+  username: DB_USERNAME_DEV,
+  password: DB_PASSWORD_DEV,
+  database: DB_NAME_DEV,
   synchronize: true,
   logging: true,
   entities: [],
